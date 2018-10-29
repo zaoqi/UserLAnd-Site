de2f1357ebfdc674c51fa274c8b556a02c804b29
Corbin Champion <corbinlc@gmail.com>
2018-10-29 16:16:17 -0700

    Updates

---
+++ precache-manifest.893d7930cbf3fa040843435125ce3646.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "03724aa403f35daaa82c21cc8f3e95d6",
+    "url": "/static/media/galaxys8-feature.03724aa4.png"
+  },
+  {
+    "revision": "51b41596a126510011d6",
+    "url": "/static/css/main.c8e43715.chunk.css"
+  },
+  {
+    "revision": "117a4e29bc6a81e005475a41fa2fa1cc",
+    "url": "/static/media/jobs.117a4e29.jpg"
+  },
+  {
+    "revision": "26bba0618093f7fbbcf2",
+    "url": "/static/js/1.26bba061.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "84ed7696e3755c0217c97586f94fed95",
+    "url": "/static/media/phone.84ed7696.png"
+  },
+  {
+    "revision": "51b41596a126510011d6",
+    "url": "/static/js/main.51b41596.chunk.js"
+  },
+  {
+    "revision": "01ad26c03fbc482d5978260d1a98477e",
+    "url": "/static/media/phone-feature-horizontal.01ad26c0.png"
+  },
+  {
+    "revision": "a5a83f1d8ed395a653f7266475ee2e1e",
+    "url": "/static/media/LogoHeader.a5a83f1d.svg"
+  },
+  {
+    "revision": "bf3dd96e3544807d94a53628c80c16cd",
+    "url": "/static/media/bg-shape.bf3dd96e.svg"
+  },
+  {
+    "revision": "b5b77e6b6dff4e3cfa48c4e9151cec8a",
+    "url": "/static/media/white and gray shapes.b5b77e6b.svg"
+  },
+  {
+    "revision": "26bba0618093f7fbbcf2",
+    "url": "/static/css/1.c9506576.chunk.css"
+  },
+  {
+    "revision": "05f0857acaef2fd6f9f2359ec688b527",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/UserLAnd-Site/precache-manifest.5f8bbda290acf07fa667142a5d07a39d.js"
+  "/precache-manifest.893d7930cbf3fa040843435125ce3646.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/UserLAnd-Site/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
