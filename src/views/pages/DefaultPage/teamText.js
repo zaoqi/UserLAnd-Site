d71d3a1deeead30973201080d9182d09ea4a7b66
Corbin Champion <corbinlc@gmail.com>
2018-12-14 12:42:09 -0800

    improve team page

--- src/views/pages/DefaultPage/teamText.js
+++ src/views/pages/DefaultPage/teamText.js
@@ -2,9 +2,13 @@ const teamText = `
 # Out Team:
 
 Corbin Champion - GM
+
 Matthew Tighe - Software Engineer
+
 Andrew Scibek - Software Engineer
+
 Thomas Luong - Software Engineer
+
 Stephen Prater - Software Engineer
 `;
 
